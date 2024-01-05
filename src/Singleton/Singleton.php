f1750cb5e77aa7d98e26b99ce784dad76aa6ca9d
Tony Sanchez <tony-sanchez@hotmail.fr>
2024-01-05 17:34:41 +0100

    Exercice singleton fait

---
+++ src/Singleton/Singleton.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Singleton;
+
+class Singleton
+{
+    private static Singleton $instance;
+
+    private function __construct()
+    {
+        
+    }
+
+    public static function getInstance(): Singleton
+    {
+        if(!isset(self::$instance)) {
+            self::$instance = new Singleton();
+        }
+        return self::$instance;
+    }
+}
